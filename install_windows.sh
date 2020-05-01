f7fa211cbb6ad957bfa20a16ddf563a8c323bbe1
David Moreau <david.moreau@jxdm.fr>
2020-05-01 09:36:47 +0200

    vscode settings on windows

---
+++ install_windows.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+cp config/vscode_settings.json /C/Users/dmoreau/AppData/Roaming/Code/User/settings.json 
+cp config/vscode_keybindings.json /C/Users/dmoreau/AppData/Roaming/Code/User/keybindings.json 
+cp config/gitconfig /C/Users/dmoreau/.gitconfig
